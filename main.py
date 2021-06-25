a6511cc1096773da9a3ec199a8521e6a85768174
umar_m <umar.m@eng.pdn.ac.lk>
2021-06-25 06:08:51 +0530

    0-1 normalise the images

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-from loading_functions import load_matlab_struct
+from map_functions import load_maps, view_maps
+from data_functions import min_max_normalise
 
 folder = 'mat_files'
 # files = ['imageKandy', 'refImage']
@@ -10,7 +11,12 @@ files = ['imageKandy']
 def main():
     dc = dict()
     for file in files:
-        dc[file] = load_matlab_struct(folder, file, view=True)
+        dc[file] = load_maps(folder, file, view=True)
+        dc_init = dc[file]
+        dc_norm = min_max_normalise(dc_init, 'all')
+        array_init = np.array(list(dc_init.values()))
+        array_norm = np.array(list(dc_norm.values()))
+        print('haha')
 
 
 if __name__ == "__main__":
