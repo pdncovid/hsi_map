4a27e1a9242cc99e45452a88b58744700fdee074
umar_m <umar.m@eng.pdn.ac.lk>
2021-08-02 07:06:20 +0530

    commented all functions

--- fn/data_functions.py
+++ fn/data_functions.py
@@ -5,6 +5,9 @@ import math
 
 
 def min_max_normalise(data_init, axis='all'):
+    """0-1 normalises each 2D array in the stacked array
+    input and output shapes are the same."""
+
     def f__(data_, axis_):
         if not axis_ == 'all':
             for k in range(data_.shape[axis_]):  # TODO this part does not work. use axis='all' for now
@@ -26,24 +29,8 @@ def min_max_normalise(data_init, axis='all'):
     return data
 
 
-def tile(data_, rows_, cols_):
-    data__ = np.copy(data_)
-    y_size = data__.shape[0] // rows_
-    x_size = data__.shape[1] // cols_
-    samples = rows_ * cols_
-    tiles_ = np.zeros((y_size, x_size, samples))
-    sample = 0
-    for x in range(cols_):
-        for y in range(rows_):
-            x_ = x * x_size
-            y_ = y * y_size
-            temp_ = data__[y_:y_ + y_size, x_:x_ + x_size]
-            tiles_[:, :, sample] = temp_
-            sample += 1
-    return tiles_
-
-
 def plot_samples(data, title, num=10, cols=5):
+    """Plots samples (each tile). Not all tiles are plotted cause its a lot of tiles then."""
 
     def plot_img(data_, title_, num_, cols_):
         if num_ > data_.shape[-1]:
@@ -79,8 +66,34 @@ def plot_samples(data, title, num=10, cols=5):
     return
 
 
+def tile(data_, rows_, cols_):
+    """Gets tiles (smaller 2D arrays) from a large 2D array according to no. of rows and columns.
+    Similar to cutting a large square cake into equal sized pieces.
+    input shape = (image shape)
+    output shape = (sample shape) * no. of samples"""
+    data__ = np.copy(data_)
+    y_size = data__.shape[0] // rows_
+    x_size = data__.shape[1] // cols_
+    samples = rows_ * cols_
+    tiles_ = np.zeros((y_size, x_size, samples))
+    sample = 0
+    for x in range(cols_):
+        for y in range(rows_):
+            x_ = x * x_size
+            y_ = y * y_size
+            temp_ = data__[y_:y_ + y_size, x_:x_ + x_size]
+            tiles_[:, :, sample] = temp_
+            sample += 1
+    return tiles_
+
+
 def samples_get(data_initial, par, view=False, print_=False):
-    # FUNCTION TO OBTAIN SAMPLES FROM THE ORIGINAL DATASET
+    """Obtains samples from the original dataset.
+    Number of samples is defined by par which is rows and columns (how to divide the big image to sample).
+    input shape = stack size * (image shape)
+    output shape = stack size * (sample shape) * no. of samples
+    stack size may be None if only 1 map (frequency band) is passed as input."""
+
     if type(par) is int:
         rows, cols = par, par
     elif type(par) is list or type(par) is np.ndarray:
@@ -120,7 +133,11 @@ def samples_get(data_initial, par, view=False, print_=False):
 
 
 def samples_zeros(data, view=False, print_=False):
-    # FUNCTION TO REMOVE FULL ZERO SAMPLES FROM THE SAMPLED DATASET
+    """Function to remove full-zero samples from the sampled dataset.(retains only the A-shaped kandy ones)
+    input shape = stack size * (sample shape) * no.samples before removing zeros
+    output shape = stack size * (sample shape) * no.samples after removing zeros.
+    stack size may be None if only 1 map (frequency band) is passed as input."""
+
     stacked = True if len(data.shape) == 4 else False
     data_ = np.copy(data)
     zero_ = []
@@ -140,4 +157,4 @@ def samples_zeros(data, view=False, print_=False):
         title = 'reduced samples (removed zeros)'
         plot_samples(data__, title, 30, 5)
 
-    return data__
+    return data__, zero_idx
