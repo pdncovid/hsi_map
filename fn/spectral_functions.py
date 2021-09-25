c1dacdbaa12ac3e321ea673a491b8716c684f5c3
Umar Marikkar <umar.m@eng.pdn.ac.lk>
2021-09-25 17:17:04 +0100

    spectral clustering initialization. affinity matrices calculated and sigma sweep carried out.

--- fn/spectral_functions.py
+++ fn/spectral_functions.py
@@ -40,7 +40,7 @@ def get_affinity(mat, sig, diag_zero=True, compute_dist=False, view_mat=True, vi
     affinity_mat = np.exp(-1 * (dist_mat ** 2) / (2 * (sig ** 2)))
 
     if diag_zero:
-        np.fill_diagonal(affinity_mat, 0)
+        affinity_mat = affinity_mat - np.identity(affinity_mat.shape[0])
 
     if view_mat:
         plt.figure(figsize=(8, 4))
@@ -59,9 +59,10 @@ def get_affinity(mat, sig, diag_zero=True, compute_dist=False, view_mat=True, vi
     return affinity_mat
 
 
-def get_laplacian(mat, normalize=True, degree_int=True, symmetry=True, view=True):
+def get_laplacian(mat, tutorial=True, normalize=True, degree_int=True, symmetry=True, view=True):
     print('computing laplacian..')
 
+    # creating degree matrix. sum of values in each row of affinity matrix.
     degree = np.zeros_like(mat)
     for i in range(degree.shape[0]):
         degree[i, i] = np.sum(mat[i, :])
@@ -69,8 +70,11 @@ def get_laplacian(mat, normalize=True, degree_int=True, symmetry=True, view=True
     if normalize:
         if symmetry:
             degree_sq = (np.linalg.inv(degree)) ** 0.5
-            # laplacian = np.identity(mat.shape[0]) - np.matmul(np.matmul(degree_sq, mat), degree_sq)
-            laplacian = np.matmul(np.matmul(degree_sq, mat), degree_sq)
+            """ LOOK HERE TO SEE THE COMPUTATION """
+            if tutorial:
+                laplacian = np.identity(mat.shape[0]) - np.matmul(np.matmul(degree_sq, mat), degree_sq)
+            else:
+                laplacian = np.matmul(np.matmul(degree_sq, mat), degree_sq)
         else:
             degree_sq = (np.linalg.inv(degree))
             laplacian = np.identity(mat.shape[0]) - np.matmul(degree_sq, mat)
@@ -85,13 +89,14 @@ def get_laplacian(mat, normalize=True, degree_int=True, symmetry=True, view=True
     return laplacian
 
 
-def get_eigen_gap(mat, sig, eigengap_only=False, view=True):
-    [eig_, _] = np.linalg.eig(mat)
-    # eig_ = (eig_ - np.amin(eig_)) / (np.amax(eig_) - np.amin(eig_))
-    eigen_gap = np.abs(np.diff(eig_))
+def get_eigen(mat, sig, eigengap_only=False, view=True):
+    [eig_val, eig_vec] = np.linalg.eig(mat)
+    # eig_val = np.abs(eig_val)
+    eigen_gap = np.diff(eig_val)
+
     if view:
         plt.figure(figsize=(12, 4))
-        plt.subplot(121), plt.stem(eig_), plt.xlim([-1, 10]), plt.ylim([0, 1])
+        plt.subplot(121), plt.stem(eig_val), plt.xlim([-1, 10]), plt.ylim([0, 1])
         plt.title('eigen-values for \u03C3 = ' + str(round(sig, 2)))
         plt.xlabel('eigen-index'), plt.ylabel('eigen-value')
         plt.subplot(122), plt.stem(eigen_gap), plt.xlim([-1, 10]), plt.ylim([0, 1])
@@ -102,4 +107,4 @@ def get_eigen_gap(mat, sig, eigengap_only=False, view=True):
     if eigengap_only:
         return eigen_gap
     else:
-        return eigen_gap, eig_
+        return eigen_gap, eig_val, eig_vec
