6c39514e9d5d103a654fb02a0be814f12b3d35d5
Erandi Attygalla <80662876+erandiatty@users.noreply.github.com>
2021-11-04 22:44:14 +0530

    Map GN boundries onto the satellite image

--- Erandi/main.py
+++ Erandi/main.py
@@ -1,20 +1,18 @@
 from pymatreader import read_mat
 from matplotlib import pyplot as plt
+import geopandas as gpd
 import numpy as np
 import functions
 
 data = read_mat("refImage.mat")
-"""How are these points obtained? 
-Please read them through a .csv file such that one can edit it anytime"""
+"""Longitude and Latitude extent of the satellite image"""
 points = [80.602, 7.332, 80.673, 7.254]
 
+kandy = functions.map_boundries_to_satellite_image(data, points)
+
 # functions.map_gps_crd(data, points)
-"""please try to generalise these functions as much as possible. 
-Send 'places, refImage, nir2' as kwargs. 
-Then if one wants to map something else they could do it as well.
-Otherwise we'd need to have another function for 'CoastalBlue' etc."""
-functions.map_places(data, points)
 # kandy = functions.geopandas_kandy()
+# functions.map_gps_crd(data, points)
 
 
 
